5a333c703542c01c507c0eb4a901f944d6a7f102
Mikkel <mikkel.h.sandberg@gmail.com>
2016-04-12 23:27:47 -0700

    Adding new icons; starting responsive styling

--- js/page.js
+++ js/page.js
@@ -4,35 +4,35 @@ $(window).resize(function() {
 	heroHeight = $('.splashSection').height();
 });
 
-$(window).scroll(function() {
-	if ($(window).scrollTop() <= heroHeight) {	
-		if ($(window).scrollTop() === 0) {
-			$('#Headphones', $('#homeIcon')[0].contentDocument)
-				.css('transform', 'rotate(0deg)');
+// $(window).scroll(function() {
+// 	if ($(window).scrollTop() <= heroHeight) {	
+// 		if ($(window).scrollTop() === 0) {
+// 			$('#Headphones', $('#homeIcon')[0].contentDocument)
+// 				.css('transform', 'rotate(0deg)');
 
-				$('#homeIcon').css('left', 0);
+// 				$('#homeIcon').css('left', 0);
 
-			$('#apc-icon > g:not(#Headphones)', $('#homeIcon')[0].contentDocument)
-				.css('opacity', '1');
-		} else if ($(window).scrollTop() > heroHeight - 20) {
-			$('#Headphones', $('#homeIcon')[0].contentDocument)
-				.css('transform', 'rotate(90deg)');
+// 			$('#apc-icon > g:not(#Headphones)', $('#homeIcon')[0].contentDocument)
+// 				.css('opacity', '1');
+// 		} else if ($(window).scrollTop() > heroHeight - 20) {
+// 			$('#Headphones', $('#homeIcon')[0].contentDocument)
+// 				.css('transform', 'rotate(90deg)');
 
-			$('#homeIcon').css({'left': -20, 'top': 7});
+// 			$('#homeIcon').css({'left': -20, 'top': 7});
 
-			$('#apc-icon > g:not(#Headphones)', $('#homeIcon')[0].contentDocument)
-				.css('opacity', '0');
-		} else {
-			$('#Headphones', $('#homeIcon')[0].contentDocument)
-				.css('transform', 'rotate(' + $(window).scrollTop() / (heroHeight / 90) + 'deg)');
+// 			$('#apc-icon > g:not(#Headphones)', $('#homeIcon')[0].contentDocument)
+// 				.css('opacity', '0');
+// 		} else {
+// 			$('#Headphones', $('#homeIcon')[0].contentDocument)
+// 				.css('transform', 'rotate(' + $(window).scrollTop() / (heroHeight / 90) + 'deg)');
 
-			$('#homeIcon').css({'left': $(window).scrollTop() / (heroHeight / -20), 'top': $(window).scrollTop() / (heroHeight / 7)});
+// 			$('#homeIcon').css({'left': $(window).scrollTop() / (heroHeight / -20), 'top': $(window).scrollTop() / (heroHeight / 7)});
 
-			$('#apc-icon > g:not(#Headphones)', $('#homeIcon')[0].contentDocument)
-				.css('opacity', 1 / $(window).scrollTop() * 10);
-		}
-	}
-});
+// 			$('#apc-icon > g:not(#Headphones)', $('#homeIcon')[0].contentDocument)
+// 				.css('opacity', 1 - $(window).scrollTop()/heroHeight);
+// 		}
+// 	}
+// });
 /***** End icon animation *****/
 
 /***** Begin contact form verify *****/
