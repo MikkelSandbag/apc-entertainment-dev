7d795aa9844bc8592e8b9f14f3f0cea10b5d0c11
Mikkel Sandberg <Mikkel Sandberg>
2016-06-30 00:25:06 -0700

    fixing some issues with calendar responsiveness; adding favicon

--- js/calendar-init.js
+++ js/calendar-init.js
@@ -1,20 +1,59 @@
+var ww = window.innerWidth;
+var view;
+var getWindowWidth = function() {
+  return window.innerWidth;
+}
+var getView = function() {
+   return (ww < 768) ? 'basicWeek' : 'month';
+}
+  
 $(document).ready(function() {
   $('#calendar').fullCalendar({
     googleCalendarApiKey: 'AIzaSyCWrRvtyHqWOJ3M0_irYnO1wBJvZPsoQyE',
     events: {
-      googleCalendarId: '84pqfnmiq3i0us0u37ubktuem4@group.calendar.google.com',
+      googleCalendarId: 'apcentertainment@outlook.com',
       className: 'apc-ent-event',
       backgroundColor: '#ff7d7d',
-      allDayDefault: true,
-      rendering: 'background'
+      borderColor: 'transparent'
+    },
+    eventRender: function(event, element) {
+      element.attr('href', '/');
+    },
+    eventClick: function(event) {
+      if (event.url) {
+        return false;
+      }
     },
     header: {
     	left: 'prev',
     	center: 'title',
     	right: 'next'
     },
-    columnFormat: 'ddd',
-    handleWindowResize: false,
-    height: 540
+    dayNames: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    dayNamesShort: ['U', 'M', 'T', 'W', 'R', 'F', 'S'],
+    views: {
+      basicWeek: {
+        columnFormat: 'ddd'
+      },
+      month: {
+        columnFormat: 'dddd'
+      }
+    },
+    defaultView: getView(),
+    aspectRatio: function() {
+      if (getWindowWidth() < 768) {
+        return 2;
+      } else {
+        return 1;
+      }
+    },
+    windowResize: function(view) {
+      ww = getWindowWidth();
+      view = getView();
+      var currentView = $('#calendar').fullCalendar('getView');
+      if(view != currentView){
+        $('#calendar').fullCalendar('changeView', view);
+      }
+    }
   });
 });
--- js/page.js
+++ js/page.js
@@ -1,40 +1,3 @@
-/***** Begin icon animation *****/
-var heroHeight = $('.splashSection').height();
-$(window).resize(function() {
-	heroHeight = $('.splashSection').height();
-});
-
-// $(window).scroll(function() {
-// 	if ($(window).scrollTop() <= heroHeight) {	
-// 		if ($(window).scrollTop() === 0) {
-// 			$('#Headphones', $('#homeIcon')[0].contentDocument)
-// 				.css('transform', 'rotate(0deg)');
-
-// 				$('#homeIcon').css('left', 0);
-
-// 			$('#apc-icon > g:not(#Headphones)', $('#homeIcon')[0].contentDocument)
-// 				.css('opacity', '1');
-// 		} else if ($(window).scrollTop() > heroHeight - 20) {
-// 			$('#Headphones', $('#homeIcon')[0].contentDocument)
-// 				.css('transform', 'rotate(90deg)');
-
-// 			$('#homeIcon').css({'left': -20, 'top': 7});
-
-// 			$('#apc-icon > g:not(#Headphones)', $('#homeIcon')[0].contentDocument)
-// 				.css('opacity', '0');
-// 		} else {
-// 			$('#Headphones', $('#homeIcon')[0].contentDocument)
-// 				.css('transform', 'rotate(' + $(window).scrollTop() / (heroHeight / 90) + 'deg)');
-
-// 			$('#homeIcon').css({'left': $(window).scrollTop() / (heroHeight / -20), 'top': $(window).scrollTop() / (heroHeight / 7)});
-
-// 			$('#apc-icon > g:not(#Headphones)', $('#homeIcon')[0].contentDocument)
-// 				.css('opacity', 1 - $(window).scrollTop()/heroHeight);
-// 		}
-// 	}
-// });
-/***** End icon animation *****/
-
 /***** Begin mobile nav menu *****/
 $('.mobileMenuButton').click(function() {
 	$(this).next('.navInnerWrapper').toggleClass('show');
@@ -55,21 +18,7 @@ $('.navItem a').click(function() {
 $('#booking .ctaButtonInnerWrapper a').click(function(e) {
 	e.preventDefault();
 
-	$('body, .bookingPopupOverlay, .bookingPopupOuterWrapper').toggleClass('showPopup');
-});
-
-$('.bookingPopupOverlay').click(function() {
-	$('body, .bookingPopupOverlay, .bookingPopupOuterWrapper').toggleClass('showPopup');
-});
-
-$('.bookingPopupOuterWrapper .closeButton').click(function() {
- 	$('body, .bookingPopupOverlay, .bookingPopupOuterWrapper').toggleClass('showPopup');
-});
-
-$(document).on('keyup', function(e) {
-	if (e.keyCode === 27) {
-		$('body, .bookingPopupOverlay, .bookingPopupOuterWrapper').removeClass('showPopup');
-	}
+	$('#bookingFormAnchor').toggleClass('showPopup');
 });
 
 $('#eventForm button').addClass('disabled').attr('disabled', true);
@@ -290,5 +239,3 @@ $('.contactFormSection input, .contactFormSection textarea').blur(function(event
 	}
 });
 /***** End contact form verify *****/
-
-/***** Begin calendar display adjustments *****/
